ea31bd7b03c4acf7620d3a1ce2848a51065fc521
Ali S <maryasharman@gmail.com>
2016-03-26 00:08:34 -0400

    Updated files to have custom 'hamburger' type icon for small screen and adjust layouts. Added custom font. Built special button for small screen resources.

--- static/jscript.js
+++ static/jscript.js
@@ -6,3 +6,43 @@ $(function(){
     }
   });
 });
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content. From: http://www.w3schools.com/howto/howto_js_dropdown.asp */
+/* move left from: http://stackoverflow.com/questions/23593929/html-javascript-move-image-by-onmouseover-on-a-button*/
+function myFunction() {
+    var butn = document.getElementsByClassName("dropbtn")[0];
+    document.getElementById("myDropdown").classList.toggle("show");
+    if (document.getElementById("myDropdown").clientWidth==0) {
+      butn.style.position="relative";
+      document.getElementById("content2").style.padding= "0 0 0 0";
+    }
+    else {
+      butn.style.position="absolute";
+      document.getElementById("content2").style.padding= "4em 4em 4em 4em";
+    }
+
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var butn = document.getElementsByClassName("dropbtn")[0];
+    if (document.getElementById("myDropdown").clientWidth>0) {
+      butn.style.position="relative";
+      document.getElementById("content2").style.padding= "0 0 0 0";
+    }
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
